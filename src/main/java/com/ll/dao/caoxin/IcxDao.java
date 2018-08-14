b6fb030382a69f2046201dd1fac9b40621afefcd
caoxin <2668937319@qq.com>
2018-08-14 23:51:23 +0800

    曹鑫 dao

--- src/main/java/com/ll/dao/caoxin/IcxDao.java
+++ src/main/java/com/ll/dao/caoxin/IcxDao.java
@@ -1,26 +1,40 @@
 package com.ll.dao.caoxin;
 
 import com.ll.pojo.caoxin.Caogao;
+import com.ll.pojo.caoxin.User;
 import com.ll.pojo.caoxin.WoDeProject;
+import com.ll.pojo.lin.ShenQing;
 import org.apache.ibatis.annotations.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.util.List;
-
 @Mapper
 public interface IcxDao {
     @Select("select * from WoDeProject")
     List<WoDeProject> queryList();
-
     @Delete("delete from Caogao where xiangid in (#{ids})")
     void deleteCaogao(@Param(value = "ids")String ids);
 
-
-
-    @Select("select count(*) from Caogao")
-    Long tctota(Caogao caogao);
-    @Select("select * from Caogao where 1=1  LIMIT #{start},#{end}")
-    List<Caogao> queryCaogao(@Param(value = "caogao") Caogao caogao,@Param(value = "start") Integer offset, @Param(value = "end")Integer limit);
-    @Insert("insert into Caogao(xiangid,bianhao,biaoti,fenlei,liuchang,bianjishiqi) values (#{xiangid},#{bianhao},#{biaoti},#{fenlei},#{liuchang},#{bianjishiqi})")
+    @Insert("insert into Caogao  values (#{xiangid},#{bianhao},#{biaoti},#{fenlei},#{liuchang},now())")
     void insertorig(Caogao caogao);
+    @Select("SELECT * FROM USER t WHERE t.username=#{username}")
+    User queryUserByName(@Param("username")String username);
+    @Insert("insert into user  values (#{userid},#{username},#{usersex},#{userbirth},#{userphone},#{userofficephone},#{userqq},#{usermailbox},#{useraddress},#{qiyeweixinhao},#{userimg},#{pwd})")
+    void insertUser(User user);
+    @Select("SELECT * FROM USER t WHERE t.userid=#{userid}")
+    User geren(String userid);
+
+    //申请表未提交
+    @Select("select count(*) from ll_shenqing ls,ll_role lr  where proceType=0 AND lr.roleid=ls.procerole ")
+    Long tctota(ShenQing shengqing);
+    @Select("select ls.*,lr.roletext from ll_shenqing ls,ll_role lr  where proceType=0 AND lr.roleid=ls.procerole LIMIT #{start},#{end}")
+    List<ShenQing> queryCaogao(@Param("shengqing") ShenQing shengqing,@Param("start") Integer offset, @Param("end")Integer limit);
+
+
+
+    //申请表已提交
+    @Select("select count(*) from ll_shenqing ls,ll_role lr  where proceType=1 AND lr.roleid=ls.procerole")
+    Long tctotas(ShenQing shengqing);
+    @Select("select ls.*,lr.roletext from ll_shenqing ls,ll_role lr  where proceType=1 AND lr.roleid=ls.procerole LIMIT #{start},#{end}")
+    List<ShenQing> queryshen(@Param("shengqing") ShenQing shengqing,@Param("start") Integer offset, @Param("end")Integer limit);
+    @Update("update ll_shenqing set proceType= 1 where proceid=#{ proceid}")
+    void updateProce(@Param("proceid") String proceid);
 }
