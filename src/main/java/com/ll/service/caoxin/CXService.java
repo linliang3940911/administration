8e13b87ef057c2842bcbb6ff724c39979f5a98df
caoxin <2668937319@qq.com>
2018-08-14 23:54:44 +0800

    曹鑫service

--- src/main/java/com/ll/service/caoxin/CXService.java
+++ src/main/java/com/ll/service/caoxin/CXService.java
@@ -1,18 +1,38 @@
 package com.ll.service.caoxin;
 
 import com.ll.pojo.caoxin.Caogao;
+import com.ll.pojo.caoxin.User;
 import com.ll.pojo.caoxin.WoDeProject;
+import com.ll.pojo.lin.ShenQing;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.jws.WebService;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public interface CXService {
     List<WoDeProject> queryList();
-
-    List<Caogao> queryCaogao(Caogao caogao,Integer offset,Integer limit);
-
     void deleteCaogao(String ids);
-
-    Long tctota(Caogao caogao);
-
     void insertorig(Caogao caogao);
+
+
+    Map<String,Object> login(User user);
+
+    String uploadImg(MultipartFile imgs) throws IOException;
+
+    void insertUser(User user);
+    User geren(String userid);
+
+
+//草稿箱未提交
+    List<ShenQing> queryCaogao(ShenQing shengqing,Integer offset,Integer limit);
+    Long tctota(ShenQing shengqing);
+
+
+    //草稿箱已提交
+    Long tctotas(ShenQing shengqing);
+    List<ShenQing> queryshen(ShenQing shengqing, Integer offset, Integer limit);
+
+    void updateProce(String proceid);
 }
--- src/main/java/com/ll/service/caoxin/CXServiceImpl.java
+++ src/main/java/com/ll/service/caoxin/CXServiceImpl.java
@@ -2,13 +2,18 @@ package com.ll.service.caoxin;
 
 import com.ll.dao.caoxin.IcxDao;
 import com.ll.pojo.caoxin.Caogao;
+import com.ll.pojo.caoxin.User;
 import com.ll.pojo.caoxin.WoDeProject;
+import com.ll.pojo.lin.ShenQing;
+import com.ll.util.OSSClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -22,8 +27,8 @@ public class CXServiceImpl implements CXService {
     }
 
     @Override
-    public List<Caogao> queryCaogao(Caogao caogao, Integer offset, Integer limit) {
-        return cxdao.queryCaogao(caogao,offset,limit);
+    public List<ShenQing> queryCaogao(ShenQing shengqing, Integer offset, Integer limit) {
+        return cxdao.queryCaogao(shengqing,offset,limit);
     }
 
     @Override
@@ -32,16 +37,77 @@ public class CXServiceImpl implements CXService {
     }
 
     @Override
-    public Long tctota(Caogao caogao) {
-        return cxdao.tctota(caogao);
+    public Long tctota(ShenQing shengqing) {
+        return cxdao.tctota(shengqing);
+    }
+
+    @Override
+    public Long tctotas(ShenQing shengqing) {
+        return cxdao.tctotas(shengqing);
+    }
+
+    @Override
+    public List<ShenQing> queryshen(ShenQing shengqing, Integer offset, Integer limit) {
+        return cxdao.queryshen(shengqing,offset,limit);
+    }
+
+    @Override
+    public void updateProce(String proceid) {
+        cxdao.updateProce(proceid);
+    }
+
+    @Override
+    public Map<String, Object> login(User user) {
+        String flag = "0";//0 :用户名错误    1：密码错误  2：登录成功
+        Map<String, Object> map =  new  HashMap<>();
+        User user2 = cxdao.queryUserByName(user.getUsername());
+        if(user2 != null){
+            flag = "1";
+            if(user2.getPwd().equals(user.getPwd())){
+                flag = "2";
+                map.put("user2", user2);
+            }
+        }
+        map.put("flag", flag);
+        return map;
+    }
+/**
+　　* @Description: 上传
+　　* @param ${tags}
+　　* @return ${return_type}
+　　* @throws
+　　* @author 曹鑫
+　　* @date 2018-08-14 11:25
+　　*/
+    @Override
+    public String uploadImg(MultipartFile file) throws IOException {
+        if (file == null || file.getSize() <= 0) {
+            throw new IOException("file不能为空");
+        }
+        OSSClientUtil ossClient=new OSSClientUtil();
+        String name = ossClient.uploadImg2Oss(file);
+        String imgUrl = ossClient.getImgUrl(name);
+        String[] split = imgUrl.split("\\?");
+
+        return split[0];
+    }
+
+    @Override
+    public void insertUser(User user) {
+        String  uuid= UUID.randomUUID().toString();
+        user.setUserid(uuid);
+        cxdao.insertUser(user);
+    }
+
+    @Override
+    public User geren(String userid) {
+        return cxdao.geren(userid);
     }
 
     @Override
     public void insertorig(Caogao caogao) {
         String  uuid= UUID.randomUUID().toString();
         caogao.setXiangid(uuid);
-        Date date =new Date();
-        caogao.setBainjishiqi(date);
         cxdao.insertorig(caogao);
     }
 
