80c27a3a1c67044d0384f4b0db56522dcad7fd58
yinronger <2662872804@qq.com>
2018-08-20 08:47:50 +0800

    因蓉儿部门实体类

---
+++ src/main/java/com/ll/pojo/yre/DeptPojo.java
@@ -0,0 +1,41 @@
+package com.ll.pojo.yre;
+
+public class DeptPojo {
+
+    private String deptid;//
+    private String deptname;//
+    private String deptpid;//
+    private String url;//
+
+    public String getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(String deptid) {
+        this.deptid = deptid;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+
+    public String getDeptpid() {
+        return deptpid;
+    }
+
+    public void setDeptpid(String deptpid) {
+        this.deptpid = deptpid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
